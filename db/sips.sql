29c48b6b597e8dde8f65f11eb73829c1baed2376
okta467 <oktaalfiansyah@gmail.com>
2024-07-22 12:15:00 +0700

    Update database
    - [Added] table tbl_agenda
    - [Added] column `asal_surat` in table tbl_surat_masuk
    - [Added] column `kode_surat` in table tbl_kode_surat
    - [Added] column `id_pengguna` in table tbl_pegawai
    - [Added] column `tahun_ijazah` in table tbl_pegawai
    - [Added] column `pengolah_surat` in table tbl_surat_keluar
    - [Added] column `tujuan_surat` in table tbl_surat_keluar
    - [Added] data in table tbl_kode_surat
    - [Added] data in table tbl_pegawai
    - [Added] data in table tbl_surat_keluar
    - [Added] data in table tbl_surat_masuk
    - [Added] data in table tbl_tujuan_disposisi_surat_masuk-
    - [Updated] column from `nik` to `nip` in table tbl_pegawai
    - [Updated] column hak_akses from enum('admin', 'kepala_desa') to enum('admin','pengelola_surat','kepala_desa','sekretaris_desa','pegawai')
    - [Updated] data from old data to new data in tbl_jabatan
    - [Updated] data from old data to new data in tbl_pengguna
    - [Deleted] column `tmt` in table tbl_pegawai
    - [Deleted] column `pengirim_surat` in table tbl_surat_keluar
    - [Deleted] column `penerima_surat` in table tbl_surat_keluar
    - [Deleted] column `pengirim_surat` in table tbl_surat_masuk

--- db/sips.sql
+++ db/sips.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 20, 2024 at 06:14 AM
+-- Generation Time: Jul 22, 2024 at 07:14 AM
 -- Server version: 10.4.20-MariaDB
 -- PHP Version: 8.0.8
 
@@ -24,6 +24,31 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `tbl_agenda`
+--
+
+CREATE TABLE `tbl_agenda` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `tempat` varchar(1000) NOT NULL,
+  `dari_jam` time NOT NULL,
+  `sampai_jam` time NOT NULL,
+  `peserta` varchar(1000) NOT NULL,
+  `tgl_acara` date NOT NULL,
+  `detail_acara` varchar(5000) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updated_at` timestamp NULL DEFAULT NULL ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tbl_agenda`
+--
+
+INSERT INTO `tbl_agenda` (`id`, `tempat`, `dari_jam`, `sampai_jam`, `peserta`, `tgl_acara`, `detail_acara`, `created_at`, `updated_at`) VALUES
+(2, 'Halaman Masjid Ulil Albab', '13:00:00', '15:00:00', 'Semua pegawai kantor kecamatan', '2019-01-31', 'Kegiatan donor darah', '2024-07-22 00:16:32', NULL);
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `tbl_disposisi_surat_masuk`
 --
 
@@ -35,6 +60,13 @@ CREATE TABLE `tbl_disposisi_surat_masuk` (
   `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
   `updated_at` timestamp NULL DEFAULT NULL ON UPDATE current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tbl_disposisi_surat_masuk`
+--
+
+INSERT INTO `tbl_disposisi_surat_masuk` (`id`, `id_surat_masuk`, `instruksi`, `tgl_penyelesaian`, `created_at`, `updated_at`) VALUES
+(12, 1, 'Tolong Dibantu', '2018-08-27', '2024-07-22 05:11:24', NULL);
 
 -- --------------------------------------------------------
 
@@ -54,18 +86,13 @@ CREATE TABLE `tbl_jabatan` (
 --
 
 INSERT INTO `tbl_jabatan` (`id`, `nama_jabatan`, `created_at`, `updated_at`) VALUES
-(1, 'Kepala Sekolah', '2024-05-20 12:45:34', NULL),
-(2, 'Wakil Kepala Sekolah', '2024-05-20 12:45:34', NULL),
-(3, 'Bendahara', '2024-05-20 12:45:34', NULL),
-(4, 'Tata Usaha/Administrasi', '2024-05-20 12:45:34', NULL),
-(5, 'Wali Kelas', '2024-05-20 12:45:34', NULL),
-(6, 'Piket', '2024-05-20 12:45:34', NULL),
-(7, 'Bimbingan Konseling', '2024-05-20 12:45:34', NULL),
-(8, 'Penjaga Sekolah', '2024-05-20 12:45:34', NULL),
-(9, 'Kebersihan', '2024-05-20 12:45:34', '2024-05-20 12:53:45'),
-(10, 'Tenaga Administrasi Sekolah', '2024-05-20 12:45:34', NULL),
-(11, 'Perpustakaan', '2024-05-20 12:45:34', NULL),
-(12, 'Operator', '2024-05-20 12:45:34', NULL);
+(1, 'Kepala Desa', '2024-07-21 01:41:00', NULL),
+(2, 'Sekretaris Desa', '2024-07-21 01:41:00', NULL),
+(3, 'Trantip', '2024-07-21 01:41:00', NULL),
+(4, 'Kasi Ekobang', '2024-07-21 01:41:00', NULL),
+(5, 'Kasi AB', '2024-07-21 01:41:00', NULL),
+(6, 'Staff Kasi Ekobang', '2024-07-21 01:41:00', NULL),
+(7, 'Staff Kasi AB', '2024-07-21 01:41:00', NULL);
 
 -- --------------------------------------------------------
 
@@ -118,11 +145,20 @@ INSERT INTO `tbl_jurusan_pendidikan` (`id`, `id_pendidikan`, `nama_jurusan`, `cr
 
 CREATE TABLE `tbl_kode_surat` (
   `id` int(10) UNSIGNED NOT NULL,
+  `kode_surat` varchar(64) NOT NULL,
   `nama_kode` varchar(255) NOT NULL,
   `keterangan` varchar(1000) NOT NULL,
   `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
   `updated_at` timestamp NULL DEFAULT NULL ON UPDATE current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tbl_kode_surat`
+--
+
+INSERT INTO `tbl_kode_surat` (`id`, `kode_surat`, `nama_kode`, `keterangan`, `created_at`, `updated_at`) VALUES
+(1, '000', 'Umum', '', '2024-07-21 02:09:27', NULL),
+(2, '005', 'Undangan', '', '2024-07-21 02:14:09', NULL);
 
 -- --------------------------------------------------------
 
@@ -173,20 +209,30 @@ INSERT INTO `tbl_pangkat_golongan` (`id`, `nama_pangkat_golongan`, `tipe`, `crea
 
 CREATE TABLE `tbl_pegawai` (
   `id` int(10) UNSIGNED NOT NULL,
+  `id_pengguna` int(10) UNSIGNED DEFAULT NULL,
   `id_jabatan` int(10) UNSIGNED DEFAULT NULL,
   `id_pangkat_golongan` int(10) UNSIGNED DEFAULT NULL,
   `id_pendidikan` int(10) UNSIGNED DEFAULT NULL,
   `id_jurusan_pendidikan` int(10) UNSIGNED DEFAULT NULL,
-  `nik` varchar(16) NOT NULL,
+  `nip` varchar(16) NOT NULL,
   `nama_pegawai` varchar(128) NOT NULL,
   `jk` enum('l','p') NOT NULL,
   `alamat` varchar(255) NOT NULL,
   `tmp_lahir` varchar(64) NOT NULL,
   `tgl_lahir` date NOT NULL,
-  `tmt` date DEFAULT NULL,
+  `tahun_ijazah` year(4) NOT NULL,
   `created_at` timestamp NULL DEFAULT current_timestamp(),
   `updated_at` timestamp NULL DEFAULT NULL ON UPDATE current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tbl_pegawai`
+--
+
+INSERT INTO `tbl_pegawai` (`id`, `id_pengguna`, `id_jabatan`, `id_pangkat_golongan`, `id_pendidikan`, `id_jurusan_pendidikan`, `nip`, `nama_pegawai`, `jk`, `alamat`, `tmp_lahir`, `tgl_lahir`, `tahun_ijazah`, `created_at`, `updated_at`) VALUES
+(1, 47, 1, 16, 9, 4, '1234567890123456', 'Test Kepala Desa', 'l', 'Palembang', 'Palembang', '1997-01-05', 2010, '2024-07-21 07:05:18', '2024-07-21 08:27:53'),
+(2, 48, 3, 14, 7, 11, '6818385748000934', 'Test Pengelola Surat', 'l', 'Palembang', 'Palembang', '2024-07-09', 2009, '2024-07-21 07:17:08', NULL),
+(15, NULL, 7, 16, 9, 4, '4635889616676390', 'Test Tanpa Hak Akses', 'l', 'Palembang', 'Palembang', '1995-01-01', 2010, '2024-07-21 07:42:04', '2024-07-21 08:26:41');
 
 -- --------------------------------------------------------
 
@@ -228,8 +274,7 @@ CREATE TABLE `tbl_pengguna` (
   `id` int(10) UNSIGNED NOT NULL,
   `username` varchar(32) NOT NULL,
   `password` varchar(128) NOT NULL,
-  `hak_akses` enum('admin','masyarakat','kepala_desa') NOT NULL,
-  `status_aktif` tinyint(4) NOT NULL DEFAULT 0 COMMENT '1 = aktif, 0 = tidak aktif',
+  `hak_akses` enum('admin','pengelola_surat','kepala_desa','sekretaris_desa','pegawai') NOT NULL,
   `created_at` timestamp NULL DEFAULT current_timestamp(),
   `last_login` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
@@ -238,10 +283,13 @@ CREATE TABLE `tbl_pengguna` (
 -- Dumping data for table `tbl_pengguna`
 --
 
-INSERT INTO `tbl_pengguna` (`id`, `username`, `password`, `hak_akses`, `status_aktif`, `created_at`, `last_login`) VALUES
-(9, 'admin', '$2y$10$r6i9ouw57cTTevcboVpfxuaaeGE.LqvH0ivtFunGnpjhus3jtxu1q', 'admin', 1, '2024-06-10 14:42:24', '2024-07-11 06:41:18'),
-(25, '196506121990022003', '$2y$10$r6i9ouw57cTTevcboVpfxuaaeGE.LqvH0ivtFunGnpjhus3jtxu1q', 'kepala_desa', 1, '2024-06-24 18:29:06', '2024-07-11 06:41:24'),
-(35, '1989986520190220', '$2y$10$LrNrbSt9hTDGlwWP8vEpj.axs3qGn0aWFd4UYcjL.w5q79LY3Ouiy', 'kepala_desa', 1, '2024-06-27 14:46:25', '2024-07-11 06:41:06');
+INSERT INTO `tbl_pengguna` (`id`, `username`, `password`, `hak_akses`, `created_at`, `last_login`) VALUES
+(9, 'admin', '$2y$10$r6i9ouw57cTTevcboVpfxuaaeGE.LqvH0ivtFunGnpjhus3jtxu1q', 'admin', '2024-06-10 14:42:24', '2024-07-20 20:34:05'),
+(25, '196506121990022003', '$2y$10$r6i9ouw57cTTevcboVpfxuaaeGE.LqvH0ivtFunGnpjhus3jtxu1q', 'kepala_desa', '2024-06-24 18:29:06', '2024-07-11 06:41:24'),
+(35, '1989986520190220', '$2y$10$LrNrbSt9hTDGlwWP8vEpj.axs3qGn0aWFd4UYcjL.w5q79LY3Ouiy', 'kepala_desa', '2024-06-27 14:46:25', '2024-07-11 06:41:06'),
+(47, '123456789012345678', '$2y$10$Qdw3OiSzj13ztr3IwUX2GO3NE31FO9/OjoCCv8yVdjWbadA2JQfHK', 'kepala_desa', '2024-07-21 07:05:18', NULL),
+(48, '681838574800093422', '$2y$10$WNQx.dXbv6whlPzQOtJfIeh.7.G3ic.H.VJ8pEUbmLNV3vc5.vde6', 'pengelola_surat', '2024-07-21 07:17:08', NULL),
+(62, '1234567890123456', '$2y$10$j0g789mR1IH7mjVFlYUqMeLjQ4A.1KWvJipESZiKSR7Z3p9F/fY9W', 'kepala_desa', '2024-07-21 08:27:53', NULL);
 
 -- --------------------------------------------------------
 
@@ -251,9 +299,9 @@ INSERT INTO `tbl_pengguna` (`id`, `username`, `password`, `hak_akses`, `status_a
 
 CREATE TABLE `tbl_surat_keluar` (
   `id` int(10) UNSIGNED NOT NULL,
-  `id_kode_surat` int(10) UNSIGNED NOT NULL,
-  `pengirim_surat` int(10) UNSIGNED DEFAULT NULL,
-  `penerima_surat` int(10) UNSIGNED DEFAULT NULL,
+  `id_kode_surat` int(10) UNSIGNED DEFAULT NULL,
+  `pengolah_surat` varchar(255) NOT NULL,
+  `tujuan_surat` varchar(255) NOT NULL,
   `no_surat` varchar(128) NOT NULL,
   `tgl_surat` date NOT NULL,
   `perihal_indeks` varchar(255) NOT NULL,
@@ -266,6 +314,13 @@ CREATE TABLE `tbl_surat_keluar` (
   `updated_at` timestamp NULL DEFAULT NULL ON UPDATE current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Dumping data for table `tbl_surat_keluar`
+--
+
+INSERT INTO `tbl_surat_keluar` (`id`, `id_kode_surat`, `pengolah_surat`, `tujuan_surat`, `no_surat`, `tgl_surat`, `perihal_indeks`, `isi_surat`, `jml_lampiran`, `jml_lembar`, `catatan`, `file_sk`, `created_at`, `updated_at`) VALUES
+(3, 1, 'Kasubag Umpeg', 'KesbangPol', '717/Kaprodi TF/20/Penelitian/III/2018', '2024-07-22', 'Izin', 'Izin Pengambilan Data', 0, 0, 'Tidak ada', '172036e622f8a8a470345c2b93c075040b2479415238cbecaca98d558f4be852.pdf', '2024-07-22 01:27:03', '2024-07-22 02:21:41');
+
 -- --------------------------------------------------------
 
 --
@@ -274,8 +329,8 @@ CREATE TABLE `tbl_surat_keluar` (
 
 CREATE TABLE `tbl_surat_masuk` (
   `id` int(10) UNSIGNED NOT NULL,
-  `id_kode_surat` int(10) UNSIGNED NOT NULL,
-  `pengirim_surat` int(10) UNSIGNED DEFAULT NULL,
+  `id_kode_surat` int(10) UNSIGNED DEFAULT NULL,
+  `asal_surat` varchar(255) NOT NULL,
   `no_surat` varchar(128) NOT NULL,
   `tgl_surat` date NOT NULL,
   `perihal_indeks` varchar(255) NOT NULL,
@@ -285,6 +340,13 @@ CREATE TABLE `tbl_surat_masuk` (
   `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
   `updated_at` timestamp NULL DEFAULT NULL ON UPDATE current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tbl_surat_masuk`
+--
+
+INSERT INTO `tbl_surat_masuk` (`id`, `id_kode_surat`, `asal_surat`, `no_surat`, `tgl_surat`, `perihal_indeks`, `isi_surat`, `jml_lampiran`, `file_sm`, `created_at`, `updated_at`) VALUES
+(1, 1, 'Kesbangpol', '070/Kesbangpol/1371/2018', '2018-03-27', 'Surat Izin', 'Mengizinkan Pengambilan Data', 0, 'd57db0ac26dd76453fdafe7d551cdfa57311e2fcb6bb5c79fe4bab079e2f29ab.pdf', '2024-07-22 03:10:39', NULL);
 
 -- --------------------------------------------------------
 
@@ -301,8 +363,22 @@ CREATE TABLE `tbl_tujuan_disposisi_surat_masuk` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
+-- Dumping data for table `tbl_tujuan_disposisi_surat_masuk`
+--
+
+INSERT INTO `tbl_tujuan_disposisi_surat_masuk` (`id`, `id_disposisi_surat_masuk`, `id_jabatan`, `created_at`, `updated_at`) VALUES
+(13, 12, 2, '2024-07-22 05:11:24', NULL),
+(14, 12, 3, '2024-07-22 05:11:24', NULL);
+
+--
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `tbl_agenda`
+--
+ALTER TABLE `tbl_agenda`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `tbl_disposisi_surat_masuk`
@@ -341,11 +417,12 @@ ALTER TABLE `tbl_pangkat_golongan`
 --
 ALTER TABLE `tbl_pegawai`
   ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `nik` (`nik`),
+  ADD UNIQUE KEY `nik` (`nip`),
   ADD KEY `id_jabatan` (`id_jabatan`),
   ADD KEY `id_jurusan_pendidikan` (`id_jurusan_pendidikan`),
   ADD KEY `id_pangkat_golongan` (`id_pangkat_golongan`),
-  ADD KEY `id_pendidikan` (`id_pendidikan`);
+  ADD KEY `id_pendidikan` (`id_pendidikan`),
+  ADD KEY `id_pengguna` (`id_pengguna`);
 
 --
 -- Indexes for table `tbl_pendidikan`
@@ -364,13 +441,15 @@ ALTER TABLE `tbl_pengguna`
 -- Indexes for table `tbl_surat_keluar`
 --
 ALTER TABLE `tbl_surat_keluar`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_kode_surat` (`id_kode_surat`);
 
 --
 -- Indexes for table `tbl_surat_masuk`
 --
 ALTER TABLE `tbl_surat_masuk`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_kode_surat` (`id_kode_surat`);
 
 --
 -- Indexes for table `tbl_tujuan_disposisi_surat_masuk`
@@ -385,16 +464,22 @@ ALTER TABLE `tbl_tujuan_disposisi_surat_masuk`
 --
 
 --
+-- AUTO_INCREMENT for table `tbl_agenda`
+--
+ALTER TABLE `tbl_agenda`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
 -- AUTO_INCREMENT for table `tbl_disposisi_surat_masuk`
 --
 ALTER TABLE `tbl_disposisi_surat_masuk`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
 -- AUTO_INCREMENT for table `tbl_jabatan`
 --
 ALTER TABLE `tbl_jabatan`
-  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT for table `tbl_jurusan_pendidikan`
@@ -406,7 +491,7 @@ ALTER TABLE `tbl_jurusan_pendidikan`
 -- AUTO_INCREMENT for table `tbl_kode_surat`
 --
 ALTER TABLE `tbl_kode_surat`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `tbl_pangkat_golongan`
@@ -418,7 +503,7 @@ ALTER TABLE `tbl_pangkat_golongan`
 -- AUTO_INCREMENT for table `tbl_pegawai`
 --
 ALTER TABLE `tbl_pegawai`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 
 --
 -- AUTO_INCREMENT for table `tbl_pendidikan`
@@ -430,25 +515,25 @@ ALTER TABLE `tbl_pendidikan`
 -- AUTO_INCREMENT for table `tbl_pengguna`
 --
 ALTER TABLE `tbl_pengguna`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=47;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=63;
 
 --
 -- AUTO_INCREMENT for table `tbl_surat_keluar`
 --
 ALTER TABLE `tbl_surat_keluar`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `tbl_surat_masuk`
 --
 ALTER TABLE `tbl_surat_masuk`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `tbl_tujuan_disposisi_surat_masuk`
 --
 ALTER TABLE `tbl_tujuan_disposisi_surat_masuk`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 
 --
 -- Constraints for dumped tables
@@ -470,7 +555,20 @@ ALTER TABLE `tbl_jurusan_pendidikan`
 -- Constraints for table `tbl_pegawai`
 --
 ALTER TABLE `tbl_pegawai`
-  ADD CONSTRAINT `tbl_pegawai_ibfk_1` FOREIGN KEY (`id_jabatan`) REFERENCES `tbl_jabatan` (`id`) ON DELETE SET NULL ON UPDATE CASCADE;
+  ADD CONSTRAINT `tbl_pegawai_ibfk_1` FOREIGN KEY (`id_jabatan`) REFERENCES `tbl_jabatan` (`id`) ON DELETE SET NULL ON UPDATE CASCADE,
+  ADD CONSTRAINT `tbl_pegawai_ibfk_2` FOREIGN KEY (`id_pengguna`) REFERENCES `tbl_pengguna` (`id`) ON DELETE SET NULL ON UPDATE CASCADE;
+
+--
+-- Constraints for table `tbl_surat_keluar`
+--
+ALTER TABLE `tbl_surat_keluar`
+  ADD CONSTRAINT `tbl_surat_keluar_ibfk_1` FOREIGN KEY (`id_kode_surat`) REFERENCES `tbl_kode_surat` (`id`) ON DELETE SET NULL ON UPDATE CASCADE;
+
+--
+-- Constraints for table `tbl_surat_masuk`
+--
+ALTER TABLE `tbl_surat_masuk`
+  ADD CONSTRAINT `tbl_surat_masuk_ibfk_1` FOREIGN KEY (`id_kode_surat`) REFERENCES `tbl_kode_surat` (`id`) ON DELETE SET NULL ON UPDATE CASCADE;
 
 --
 -- Constraints for table `tbl_tujuan_disposisi_surat_masuk`
