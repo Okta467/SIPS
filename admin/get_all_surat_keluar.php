77ddbaf5a9175ed8fd47d043db54b9f5e98d382e
okta467 <oktaalfiansyah@gmail.com>
2024-07-22 12:28:35 +0700

    Add feature surat keluar for role admin

---
+++ admin/get_all_surat_keluar.php
@@ -0,0 +1,37 @@
+<?php
+    include_once '../helpers/isAccessAllowedHelper.php';
+
+    // cek apakah user yang mengakses adalah admin?
+    if (!isAccessAllowed('admin')) {
+        session_destroy();
+        echo "<meta http-equiv='refresh' content='0;" . base_url_return('index.php?msg=other_error') . "'>";
+        return;
+    }
+
+    include_once '../config/connection.php';
+    
+    $stmt = mysqli_stmt_init($connection);
+
+    $query = "SELECT
+            a.id AS id_surat_keluar, a.pengolah_surat, a.tujuan_surat, a.no_surat, a.tgl_surat, a.perihal_indeks, a.isi_surat, a.jml_lampiran, a.jml_lembar, a.catatan, a.file_sk,
+            b.id AS id_kode_surat, b.kode_surat, b.nama_kode, b.keterangan
+        FROM tbl_surat_keluar AS a
+        LEFT JOIN tbl_kode_surat AS b
+            ON b.id = a.id_kode_surat
+        ORDER BY a.id DESC";
+
+    mysqli_stmt_prepare($stmt, $query);
+    mysqli_stmt_execute($stmt);
+
+	$result = mysqli_stmt_get_result($stmt);
+
+    $surat_keluars['data'] = !$result
+        ? array()
+        : mysqli_fetch_all($result, MYSQLI_ASSOC);
+
+    mysqli_stmt_close($stmt);
+    mysqli_close($connection);
+
+    echo json_encode($surat_keluars);
+
+?>
---
+++ admin/surat_keluar_hapus.php
@@ -0,0 +1,52 @@
+<?php
+    include_once '../helpers/isAccessAllowedHelper.php';
+
+    // cek apakah user yang mengakses adalah admin?
+    if (!isAccessAllowed('admin')) {
+        session_destroy();
+        echo "<meta http-equiv='refresh' content='0;" . base_url_return('index.php?msg=other_error') . "'>";
+        return;
+    }
+
+    include_once '../config/connection.php';
+    
+    $id_surat_keluar = $_GET['xid_surat_keluar'];
+    
+    $stmt_surat_keluar = mysqli_stmt_init($connection);
+
+    mysqli_stmt_prepare($stmt_surat_keluar, "SELECT file_sk FROM tbl_surat_keluar WHERE id=?");
+    mysqli_stmt_bind_param($stmt_surat_keluar, 'i', $id_surat_keluar);
+    mysqli_stmt_execute($stmt_surat_keluar);
+
+    $result = mysqli_stmt_get_result($stmt_surat_keluar);
+    $surat_keluar = mysqli_fetch_assoc($result);
+
+    $stmt_hapus = mysqli_stmt_init($connection);
+
+    mysqli_stmt_prepare($stmt_hapus, "DELETE FROM tbl_surat_keluar WHERE id=?");
+    mysqli_stmt_bind_param($stmt_hapus, 'i', $id_surat_keluar);
+
+    $delete = mysqli_stmt_execute($stmt_hapus);
+    
+    // Delete file if data deletion is success
+    if ($delete) {
+        $target_dir = '../assets/uploads/file_sk/';
+        $old_file_sk = $surat_keluar['file_sk'];
+        $file_path_to_unlink = $target_dir . $old_file_sk;
+        
+        // Delete the old file_sk
+        if ($old_file_sk && file_exists($file_path_to_unlink)) {
+            unlink("{$target_dir}{$old_file_sk}");
+        }
+    }
+
+    !$delete
+        ? $_SESSION['msg'] = 'other_error'
+        : $_SESSION['msg'] = 'delete_success';
+
+    mysqli_stmt_close($stmt_surat_keluar);
+    mysqli_stmt_close($stmt_hapus);
+    mysqli_close($connection);
+
+    echo "<meta http-equiv='refresh' content='0;surat_keluar.php?go=surat_keluar'>";
+?>
