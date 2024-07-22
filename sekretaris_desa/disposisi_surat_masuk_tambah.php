659e7a536ff824413c168d10ce11619abc77fb3c
okta467 <oktaalfiansyah@gmail.com>
2024-07-22 13:13:55 +0700

    Add roles sekretaris desa

---
+++ sekretaris_desa/disposisi_surat_masuk_tambah.php
@@ -0,0 +1,128 @@
+<?php
+    include_once '../helpers/isAccessAllowedHelper.php';
+
+    // cek apakah user yang mengakses adalah sekretaris_desa?
+    if (!isAccessAllowed('sekretaris_desa')) {
+        session_destroy();
+        echo "<meta http-equiv='refresh' content='0;" . base_url_return('index.php?msg=other_error') . "'>";
+        return;
+    }
+
+    require_once '../vendor/htmlpurifier/HTMLPurifier.auto.php';
+    include_once '../config/connection.php';
+
+    // to sanitize user input
+    $config   = HTMLPurifier_Config::createDefault();
+    $purifier = new HTMLPurifier($config);
+
+    // echo "<pre>";
+    // print_r($_POST);
+    // $id_diteruskan_ke = $_POST['xid_diteruskan_ke'];
+    // echo count($id_diteruskan_ke);
+    // return;
+    
+    $id_surat_masuk       = htmlspecialchars($purifier->purify($_POST['xid_surat_masuk']));
+    $tgl_penyelesaian     = $_POST['xtgl_penyelesaian'];
+    $id_diteruskan_ke     = $_POST['xid_diteruskan_ke'];
+    $jml_id_diteruskan_ke = count($id_diteruskan_ke);
+    $instruksi            = htmlspecialchars($purifier->purify($_POST['xinstruksi']));
+
+    mysqli_autocommit($connection, false);
+
+    $success = true;
+
+    try {
+        /**
+         * disposisi_surat_masuk statement preparation and execution
+         */
+        $stmt_disposisi_sm = mysqli_stmt_init($connection);
+        $query_disposisi_sm = "INSERT INTO tbl_disposisi_surat_masuk
+        (
+            id_surat_masuk
+            , instruksi
+            , tgl_penyelesaian
+        )
+        VALUES (?, ?, ?)";
+        
+        if (!mysqli_stmt_prepare($stmt_disposisi_sm, $query_disposisi_sm)) {
+            $_SESSION['msg'] = 'Statement Disposisi Surat Masuk preparation failed: ' . mysqli_stmt_error($stmt_disposisi_sm);
+            echo "<meta http-equiv='refresh' content='0;pegawai.php?go=pegawai'>";
+            return;
+        }
+        
+        mysqli_stmt_bind_param($stmt_disposisi_sm, 'iss', $id_surat_masuk, $instruksi, $tgl_penyelesaian);
+        
+        if (!mysqli_stmt_execute($stmt_disposisi_sm)) {
+            $_SESSION['msg'] = 'Statement Disposisi Surat Masuk preparation failed: ' . mysqli_stmt_error($stmt_disposisi_sm);
+            echo "<meta http-equiv='refresh' content='0;pegawai.php?go=pegawai'>";
+            return;
+        }
+
+        /**
+         * tujuan_disposisi_surat_masuk statement preparation and execution
+         */
+        $stmt_tujuan_disposisi_sm = mysqli_stmt_init($connection);
+        $query_tujuan_disposisi_sm = "INSERT INTO tbl_tujuan_disposisi_surat_masuk
+        (
+            id_disposisi_surat_masuk
+            , id_jabatan
+        )
+        VALUES (?, ?)" . str_repeat(', (?, ?)', $jml_id_diteruskan_ke-1);
+        
+        if (!mysqli_stmt_prepare($stmt_tujuan_disposisi_sm, $query_tujuan_disposisi_sm)) {
+            $_SESSION['msg'] = 'Statement Tujuan Disposisi Surat Masuk preparation failed: ' . mysqli_stmt_error($stmt_tujuan_disposisi_sm);
+            echo "<meta http-equiv='refresh' content='0;pegawai.php?go=pegawai'>";
+            return;
+        }
+        
+        $id_disposisi_surat_masuk = mysqli_insert_id($connection);
+
+        $format = str_repeat('ii', $jml_id_diteruskan_ke);
+
+        $params = [];
+        foreach ($id_diteruskan_ke as $id) {
+            $params[] = $id_disposisi_surat_masuk;
+            $params[] = $id;
+        }
+
+        // Prepare the parameters for binding
+        $bind_params = [];
+        foreach ($params as $key => &$value) {
+            $bind_params[$key] = &$value; // Reference is required for call_user_func_array
+        }
+
+        // Add format string at the beginning
+        array_unshift($bind_params, $format);
+
+        // Bind parameters dynamically
+        call_user_func_array('mysqli_stmt_bind_param', array_merge([$stmt_tujuan_disposisi_sm], $bind_params));
+
+        if (!mysqli_stmt_execute($stmt_tujuan_disposisi_sm)) {
+            $_SESSION['msg'] = 'Statement Tujuan Disposisi Surat Masuk preparation failed: ' . mysqli_stmt_error($stmt_disposisi_sm);
+            echo "<meta http-equiv='refresh' content='0;pegawai.php?go=pegawai'>";
+            return;
+        }
+
+        // Commit the transaction if all statements succeed
+        if (!mysqli_commit($connection)) {
+            $_SESSION['msg'] = 'Transaction commit failed: ' . mysqli_stmt_error($stmt_disposisi_sm);
+            echo "<meta http-equiv='refresh' content='0;pegawai.php?go=pegawai'>";
+            return;
+        }
+    } catch (Exception $e) {
+        $success = false;
+        mysqli_rollback($connection);
+    }
+
+    mysqli_stmt_close($stmt_disposisi_sm);
+    mysqli_stmt_close($stmt_tujuan_disposisi_sm);
+
+    mysqli_autocommit($connection, true);
+    mysqli_close($connection);
+
+    !$success
+        ? $_SESSION['msg'] = 'other_error'
+        : $_SESSION['msg'] = 'save_success';
+
+    echo "<meta http-equiv='refresh' content='0;disposisi_surat_masuk.php?go=disposisi_surat_masuk'>";
+?>
---
+++ sekretaris_desa/kode_surat.php
@@ -0,0 +1,224 @@
+<?php
+include '../helpers/isAccessAllowedHelper.php';
+
+// cek apakah user yang mengakses adalah sekretaris_desa?
+if (!isAccessAllowed('sekretaris_desa')) :
+  session_destroy();
+  echo "<meta http-equiv='refresh' content='0;" . base_url_return('index.php?msg=other_error') . "'>";
+else :
+  include_once '../config/connection.php';
+?>
+
+
+  <!DOCTYPE html>
+  <html lang="en">
+
+  <head>
+    <?php include '_partials/head.php' ?>
+
+    <meta name="description" content="Data Kode Surat" />
+    <meta name="author" content="" />
+    <title>Kode Surat - <?= SITE_NAME ?></title>
+  </head>
+
+  <body class="nav-fixed">
+    <!--============================= TOPNAV =============================-->
+    <?php include '_partials/topnav.php' ?>
+    <!--//END TOPNAV -->
+    <div id="layoutSidenav">
+      <div id="layoutSidenav_nav">
+        <!--============================= SIDEBAR =============================-->
+        <?php include '_partials/sidebar.php' ?>
+        <!--//END SIDEBAR -->
+      </div>
+      <div id="layoutSidenav_content">
+        <main>
+          <!-- Main page content-->
+          <div class="container-xl px-4 mt-5">
+
+            <!-- Custom page header alternative example-->
+            <div class="d-flex justify-content-between align-items-sm-center flex-column flex-sm-row mb-4">
+              <div class="me-4 mb-3 mb-sm-0">
+                <h1 class="mb-0">Kode Surat</h1>
+                <div class="small">
+                  <span class="fw-500 text-primary"><?= date('D') ?></span>
+                  &middot; <?= date('M d, Y') ?> &middot; <?= date('H:i') ?> WIB
+                </div>
+              </div>
+
+              <!-- Date range picker example-->
+              <div class="input-group input-group-joined border-0 shadow w-auto">
+                <span class="input-group-text"><i data-feather="calendar"></i></span>
+                <input class="form-control ps-0 pointer" id="litepickerRangePlugin" value="Tanggal: <?= date('d M Y') ?>" readonly />
+              </div>
+
+            </div>
+            
+            <!-- Main page content-->
+            <div class="card card-header-actions mb-4 mt-5">
+              <div class="card-header">
+                <div>
+                  <i data-feather="hash" class="me-2 mt-1"></i>
+                  Data Kode Surat
+                </div>
+              </div>
+              <div class="card-body">
+                <table id="datatablesSimple">
+                  <thead>
+                    <tr>
+                      <th>#</th>
+                      <th>Kode Surat</th>
+                      <th>Nama Kode</th>
+                      <th>Keterangan</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <?php
+                    $no = 1;
+                    $query_kode_surat = mysqli_query($connection, "SELECT * FROM tbl_kode_surat ORDER BY id DESC");
+
+                    while ($kode_surat = mysqli_fetch_assoc($query_kode_surat)):
+                    ?>
+
+                      <tr>
+                        <td><?= $no++ ?></td>
+                        <td><?= $kode_surat['kode_surat'] ?></td>
+                        <td><?= $kode_surat['nama_kode'] ?></td>
+                        <td>
+                          <?php if (!$kode_surat['keterangan']): ?>
+
+                            <small class="text-muted">Tidak ada</small>
+                            
+                          <?php else: ?>
+                            
+                            <div class="ellipsis toggle_tooltip" title="<?= $kode_surat['keterangan'] ?>">
+                              <?= $kode_surat['keterangan'] ?>
+                            </div>
+                            
+                          <?php endif ?>
+                        </td>
+                      </tr>
+
+                    <?php endwhile ?>
+                  </tbody>
+                </table>
+              </div>
+            </div>
+            
+          </div>
+        </main>
+        
+        <!--============================= FOOTER =============================-->
+        <?php include '_partials/footer.php' ?>
+        <!--//END FOOTER -->
+
+      </div>
+    </div>
+    
+    <!--============================= MODAL INPUT KODE SURAT =============================-->
+    <div class="modal fade" id="ModalInputKodeSurat" tabindex="-1" role="dialog" aria-labelledby="ModalInputKodeSuratTitle" aria-hidden="true">
+      <div class="modal-dialog" role="document">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title" id="ModalInputKodeSuratTitle">Modal title</h5>
+            <button class="btn-close" type="button" data-bs-dismiss="modal" aria-label="Close"></button>
+          </div>
+          <form>
+            <div class="modal-body">
+              
+              <input type="hidden" id="xid_kode_surat" name="xid_kode_surat">
+            
+              <div class="mb-3">
+                <label class="small mb-1" for="xkode_surat">Kode Surat</label>
+                <input type="text" name="xkode_surat" maxlength="64" class="form-control" id="xkode_surat" placeholder="Enter kode surat" required />
+              </div>
+            
+              <div class="mb-3">
+                <label class="small mb-1" for="xnama_kode">Nama Kode</label>
+                <input type="text" name="xnama_kode" maxlength="255" class="form-control" id="xnama_kode" placeholder="Enter nama kode" required />
+              </div>
+            
+              <div class="mb-3">
+                <label class="small mb-1" for="xketerangan">Nama Kode</label>
+                <textarea class="form-control" id="xketerangan" name="xketerangan" rows="5" placeholder="" autocomplete="off"></textarea>
+              </div>
+
+            </div>
+            <div class="modal-footer">
+              <button class="btn btn-light border" type="button" data-bs-dismiss="modal">Batal</button>
+              <button class="btn btn-primary" type="submit">Simpan</button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+    <!--/.modal-input-kode-surat -->
+    
+    <?php include '_partials/script.php' ?>
+    <?php include '../helpers/sweetalert2_notify.php' ?>
+    
+    <!-- PAGE SCRIPT -->
+    <script>
+      $(document).ready(function() {
+        $('.toggle_modal_tambah').on('click', function() {
+          $('#ModalInputKodeSurat .modal-title').html(`<i data-feather="plus-circle" class="me-2 mt-1"></i>Tambah Kode Surat`);
+          $('#ModalInputKodeSurat form').attr({action: 'kode_surat_tambah.php', method: 'post'});
+
+          // Re-init all feather icons
+          feather.replace();
+          
+          $('#ModalInputKodeSurat').modal('show');
+        });
+
+
+        $('.toggle_modal_ubah').on('click', function() {
+          const id_kode_surat   = $(this).data('id_kode_surat');
+          const nama_kode = $(this).data('nama_kode');
+          
+          $('#ModalInputKodeSurat .modal-title').html(`<i data-feather="edit" class="me-2 mt-1"></i>Ubah Kode Surat`);
+          $('#ModalInputKodeSurat form').attr({action: 'kode_surat_ubah.php', method: 'post'});
+
+          $('#ModalInputKodeSurat #xid_kode_surat').val(id_kode_surat);
+          $('#ModalInputKodeSurat #xnama_kode').val(nama_kode);
+
+          // Re-init all feather icons
+          feather.replace();
+          
+          $('#ModalInputKodeSurat').modal('show');
+        });
+        
+
+        $('#datatablesSimple').on('click', '.toggle_swal_hapus', function() {
+          const id_kode_surat   = $(this).data('id_kode_surat');
+          const nama_kode = $(this).data('nama_kode');
+          
+          Swal.fire({
+            title: "Konfirmasi Tindakan?",
+            html: `Hapus data kode_surat: <strong>${nama_kode}?</strong>`,
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Ya, konfirmasi!"
+          }).then((result) => {
+            if (result.isConfirmed) {
+              Swal.fire({
+                title: "Tindakan Dikonfirmasi!",
+                text: "Halaman akan di-reload untuk memproses.",
+                icon: "success",
+                timer: 3000
+              }).then(() => {
+                window.location = `kode_surat_hapus.php?xid_kode_surat=${id_kode_surat}`;
+              });
+            }
+          });
+        });
+        
+      });
+    </script>
+
+  </body>
+
+  </html>
+
+<?php endif ?>
